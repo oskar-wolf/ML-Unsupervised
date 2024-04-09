1a371479d68f1b1b8695722d56f6d7e9ec0be047
oskar-wolf <mroskarwolf97@gmail.com>
2024-04-09 10:19:04 +0200

    feature_engineering

--- Unit_1_Introduction_to_Unsupervised_Machine_Learning_and_Feature_Engineeering/1_2_Feature_Engineering_example.py
+++ Unit_1_Introduction_to_Unsupervised_Machine_Learning_and_Feature_Engineeering/1_2_Feature_Engineering_example.py
@@ -2,45 +2,43 @@
 # Machine Learning - Unsupervised Machine Learning
 # Course Code: DLBDSMLUSL01
 
-'''
+"""
 In this example, we will see how we can use Python
 to extract added value within a data set based on dates.
-'''
-test
-#%% load the required packages
+"""
+# %% load the required packages
 import pandas as pd
 from scipy.stats import spearmanr
 from datetime import date
 import seaborn as sns
 
-#%% load the data
-dat = pd.read_csv('./data/data_visitors.csv', delimiter=' ')
+# %% load the data
+dat = pd.read_csv("data_visitors.csv", delimiter=" ")
 
-#%% glimpse at the data
+# %% glimpse at the data
 dat
 
-#%% correlate the date and the visitors
-spearmanr(dat['date'], dat['visitors'])
+# %% correlate the date and the visitors
+spearmanr(dat["date"], dat["visitors"])
 
-#%% define the date as such
-dat['date'] = pd.to_datetime(dat['date'])
+# %% define the date as such
+dat["date"] = pd.to_datetime(dat["date"])
 
-#%% extract the weekdays from the date
-dat['weekday'] = dat['date'].apply(lambda date: date.weekday())
+# %% extract the weekdays from the date
+dat["weekday"] = dat["date"].apply(lambda date: date.weekday())
 
-#%% glimpse at the data
+# %% glimpse at the data
 dat
 
-#%% extract the weekends
-dat['weekend'] = dat['weekday'].\
-    apply(lambda weekday: 0 if (weekday < 5) else 1)
+# %% extract the weekends
+dat["weekend"] = dat["weekday"].apply(lambda weekday: 0 if (weekday < 5) else 1)
 
-#%% glimpse at the data
+# %% glimpse at the data
 dat
 
-#%% correlate the weekends and the visitors
-spearmanr(dat['weekend'], dat['visitors'])
+# %% correlate the weekends and the visitors
+spearmanr(dat["weekend"], dat["visitors"])
 
-#%% visually glimpse at the data
+# %% visually glimpse at the data
 ax = sns.barplot(x="weekend", y="visitors", data=dat)
 # %%
